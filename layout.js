3af225e3b913862c0e6a97b8b5f3e308ed0e08cd
Jean <99772124+jeanmends@users.noreply.github.com>
2025-02-01 19:19:02 -0300

    remove all comments

--- layout.js
+++ layout.js
@@ -2,21 +2,12 @@ const dialog = document.querySelector("#dialog");
 const showButton = document.querySelector("#btn-open-add");
 const closeButton = document.querySelector("#close-btn");
 
-//const closeButton = document.querySelector("dialog button");
+
 
 showButton.addEventListener("click", () =>{
   dialog.style.display = 'block';
   document.querySelectorAll('input')[0].focus();
 })
-/*
-closeButton.addEventListener("click", () =>{
-  dialog.style.display = 'none';
-  let allInputs = document.querySelectorAll('input');
 
-  allInputs.forEach(element => {
-   element.value = '';
-  });
-})
-*/
 
 closeButton.addEventListener('click', closeAndClean);
